e8922deaaafa13b71cfdb1b9c2bbc4d1c0aa13c4
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2022-02-19 16:53:53 +0000

    first commit

---
+++ geo_reference_predictions.py
@@ -0,0 +1,70 @@
+"""
+@author: Steven Ndung'u' and Hubert K
+
+"""
+# ## Georeference Masks
+# Example found in the workstation workflow
+
+# The aim of this script is to convert prediction patches saved in jp,png, etc to TIF format referencing the metadata of the original satelite imagery
+
+
+import rasterio as rio
+import pathlib
+
+
+#### Convert the PNG predictions to Rasters Tif format
+
+# converts from png to tiff
+
+
+def convert_png_to_tif_and_save(input_img_png, save_path, georef_img_tif, idx):
+    # Input png image, to convert as geotiff
+    img = rio.open(str(input_img_png))
+    img = img.read(1)
+    # img = img.astype('uint16')
+    # img = np.stack((img,)*1, axis=1) #reshape to append the n channels of rgb for compatibility
+    # Input image for coordinate reference
+
+    with rio.open(
+        str(georef_img_tif)
+        + "\\"
+        + str(input_img_png).replace("jpg", "tif").split("\\")[-1]
+    ) as naip:
+        # open georeferenced.tif for writing
+
+        with rio.open(
+            str(save_path) + "/" + "tile_{}.tif".format(idx),
+            "w",
+            driver="GTiff",
+            count=1,
+            height=img.shape[0],
+            width=img.shape[1],
+            dtype=img.dtype,
+            crs=naip.crs,
+            transform=naip.transform,
+        ) as dst:
+            dst.write(img, indexes=1)
+
+
+# Set the paths
+# png images predicted path
+input_img_png = "E:\\PlotDel_Steven\\Data\\Test\\pred_image\\"
+input_img_png = pathlib.Path(input_img_png)
+
+# folder with tiles that were predicted (from original tif)
+georef_img_tif = "E:\\PlotDel_Steven\\Data\\Test\\Ref_image\\"
+georef_img_tif = pathlib.Path(georef_img_tif)
+
+# Path to save the outputs
+save_path = "E:\\PlotDel_Steven\\Data\\Test\\geo_referenced\\"
+save_path = pathlib.Path(save_path)
+input_img_png, georef_img_tif, save_path
+
+
+# Import the images, convert the to tif and save back in defined folder
+
+images = list(input_img_png.glob("*"))
+
+
+for idx, val in enumerate(images):
+    convert_png_to_tif_and_save(str(val), save_path, georef_img_tif, idx)
