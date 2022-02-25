e82666e9b37ae673ce580d07606d9ad396bde84c
Hubert Kanyamahanga <kanyamahanga.hubert@aims-senegal.org>
2022-02-25 09:34:32 +0000

    Added function to convert grayscale into rgb

--- convert_grayscale_to_rgb.py
+++ convert_grayscale_to_rgb.py
@@ -1,41 +1,25 @@
-"""
-Created on FEB 02/02 at Manobi Africa/ ICRISAT 
-
-@Contributors: Pierre C. Traore - ICRISAT/ Manobi Africa
-          Steven Ndung'u' - ICRISAT/ Manobi Africa
-          Joel Nteupe - Manobi Africa
-          John bagiliko - ICRISAT Intern
-          Rosmaelle Kouemo - ICRISAT Intern
-          Hubert Kanyamahanga - ICRISAT/ Manobi Africa
-          Glorie Wowo -  ICRISAT/ Manobi Africa
-"""
 import cv2
-import json
 import os
-import PIL
-import numpy as np
 from datetime import datetime
-from skimage import io, color
-import matplotlib.pyplot as plt
+from skimage import io
 import skimage.io as io
-import matplotlib.image as mpimg
-from tkinter import Tcl
-from utils.config import CustomConfig, PROJECT_ROOT
-from IPython import get_ipython
+from utils.config import PROJECT_ROOT
 
 # Get the project root directory
 project_path = PROJECT_ROOT
 RCNN_ROOT = os.path.abspath(project_path)
 os.chdir(RCNN_ROOT)
-
+path_to_image = PROJECT_ROOT + "results/Test/geo_referenced/debi_tiguet_image.tif"
 end_time = datetime.now()
 now = datetime.now()  # current date and time
 time = now.strftime("%m%d%Y_%H%M")
-img = cv2.imread(PROJECT_ROOT + "results/Test/geo_referenced/debi_tiguet_image.tif")  # BGR
-# img = cv2.imread(PROJECT_ROOT + "samples/split_images/tile22.tif")  # BGR
+img = cv2.imread(path_to_image)  # BGR
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 io.imsave(
-    os.path.join(PROJECT_ROOT + "results/Test/geo_referenced", "RGB_tile{}{}".format(str(time), ".tif")),
+    os.path.join(
+        PROJECT_ROOT + "results/Test/geo_referenced",
+        "RGB_{}{}{}".format(str(path_to_image).split("/")[0], str(time), ".tif"),
+    ),
     img,
 )
